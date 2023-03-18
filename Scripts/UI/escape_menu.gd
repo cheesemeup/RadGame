6a8071588445aaaf149f04422bdd3a8eadc4834f
cheese <alavillamailla@gmx.ch>
2023-03-19 00:48:45 +0100

    added continue function to escape menu

--- Scripts/UI/escape_menu.gd
+++ Scripts/UI/escape_menu.gd
@@ -1,9 +1,15 @@
 extends Control
 
-##############################################################################################################################
+# close esc menu via Continue
+func _on_button_continue_pressed():
+	Autoload.player_ui_main_reference.get_node("escape_menu_root").queue_free()
+
+# interface
+
+# keybindings
+
 # exiting the game via esc menu
 func _on_button_exit_game_pressed():
 	var exit_game_countdown = load("res://Scenes/UI/exit_game_countdown.tscn")
 	exit_game_countdown = exit_game_countdown.instantiate()
 	Autoload.player_ui_main_reference.add_child(exit_game_countdown)
-
