4eb9d482b72870b804b6ebb9e89fa4483cc1b162
cheese <alavillamailla@gmx.ch>
2023-04-12 23:56:50 +0200

    interface menu is now checked for int validity

--- Scripts/UI/interface_menu.gd
+++ Scripts/UI/interface_menu.gd
@@ -5,16 +5,28 @@ extends Control
 func _ready():
 	# player frame
 	var playerframe = Autoload.player_ui_main_reference.get_node("ui_persistent").get_node("playerframe").get_node("playerframe_hpbar")
-	$"interface_menu_tabs/Single Frames/playerframe_width_entry".text = str(playerframe.size.x)
-	$"interface_menu_tabs/Single Frames/playerframe_height_entry".text = str(playerframe.size.y)
-	$"interface_menu_tabs/Single Frames/playerframe_h_position_entry".text = str(playerframe.position.x)
-	$"interface_menu_tabs/Single Frames/playerframe_v_position_entry".text = str(playerframe.position.y)
-	print(Autoload.player_ui_main_reference.get_children())
-	print(Autoload.player_ui_main_reference.get_child(0))
-	print(Autoload.player_ui_main_reference.get_node("ui_persistent").get_node("playerframe").get_node("playerframe_hpbar").size.x)
-	pass # Replace with function body.
+	$"interface_menu_tabs/Single Frames/values/playerframe_width_entry".text = str(playerframe.size.x)
+	$"interface_menu_tabs/Single Frames/values/playerframe_height_entry".text = str(playerframe.size.y)
+	$"interface_menu_tabs/Single Frames/values/playerframe_h_position_entry".text = str(playerframe.position.x)
+	$"interface_menu_tabs/Single Frames/values/playerframe_v_position_entry".text = str(playerframe.position.y)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func _on_interface_menu_apply_pressed():
+	var playerframe = Autoload.player_ui_main_reference.get_node("ui_persistent").get_node("playerframe").get_node("playerframe_hpbar")
+	var apply_validity = apply_int_check(get_node("interface_menu_tabs").get_node("Single Frames").get_node("values"))
+	print(apply_validity)
+	if not apply_validity:
+		return
+	playerframe.size.x = int($"interface_menu_tabs/Single Frames/values/playerframe_width_entry".text)
+	playerframe.size.y = int($"interface_menu_tabs/Single Frames/values/playerframe_height_entry".text)
+	playerframe.position.x = int($"interface_menu_tabs/Single Frames/values/playerframe_h_position_entry".text)
+	playerframe.position.y = int($"interface_menu_tabs/Single Frames/values/playerframe_v_position_entry".text)
+
+func apply_int_check(value_node):
+	for child in value_node.get_children():
+		var text = child.text
+		if ! text.is_valid_int():
+			print("ERROR: invalid integer!")
+			return false
+	return true
