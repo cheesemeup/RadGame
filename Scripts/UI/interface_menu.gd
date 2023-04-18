d959adfcf7be4703ff3a6e56e1b04fc68e470517
cheese <alavillamailla@gmx.ch>
2023-04-18 02:51:03 +0200

    added orientation dropdown to player hp, so hp bar can be freely positioned, resized and have fill orientation changed

--- Scripts/UI/interface_menu.gd
+++ Scripts/UI/interface_menu.gd
@@ -5,23 +5,29 @@ extends Control
 func _ready():
 	# player frame
 	var playerframe = Autoload.player_ui_main_reference.get_node("ui_persistent").get_node("playerframe").get_node("playerframe_hpbar")
-	$"interface_menu_tabs/Single Frames/values/playerframe_width_entry".text = str(playerframe.size.x)
-	$"interface_menu_tabs/Single Frames/values/playerframe_height_entry".text = str(playerframe.size.y)
-	$"interface_menu_tabs/Single Frames/values/playerframe_h_position_entry".text = str(playerframe.position.x)
-	$"interface_menu_tabs/Single Frames/values/playerframe_v_position_entry".text = str(playerframe.position.y)
-
+	$"interface_menu_tabs/Single Frames/values/playerframe_hp_width_entry".text = str(playerframe.size.x)
+	$"interface_menu_tabs/Single Frames/values/playerframe_hp_height_entry".text = str(playerframe.size.y)
+	$"interface_menu_tabs/Single Frames/values/playerframe_hp_h_position_entry".text = str(playerframe.position.x)
+	$"interface_menu_tabs/Single Frames/values/playerframe_hp_v_position_entry".text = str(playerframe.position.y)
+	$"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_showtoggle".add_item("Show")
+	$"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_showtoggle".add_item("Hide")
+	$"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_orientation_entry".add_item("L to R")
+	$"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_orientation_entry".add_item("R to L")
+	$"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_orientation_entry".add_item("T to B")
+	$"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_orientation_entry".add_item("B to T")
 
 
 func _on_interface_menu_apply_pressed():
 	var playerframe = Autoload.player_ui_main_reference.get_node("ui_persistent").get_node("playerframe").get_node("playerframe_hpbar")
 	var apply_validity = apply_int_check(get_node("interface_menu_tabs").get_node("Single Frames").get_node("values"))
-	print(apply_validity)
+	# return if not all values are convertable to int
 	if not apply_validity:
 		return
-	playerframe.size.x = int($"interface_menu_tabs/Single Frames/values/playerframe_width_entry".text)
-	playerframe.size.y = int($"interface_menu_tabs/Single Frames/values/playerframe_height_entry".text)
-	playerframe.position.x = int($"interface_menu_tabs/Single Frames/values/playerframe_h_position_entry".text)
-	playerframe.position.y = int($"interface_menu_tabs/Single Frames/values/playerframe_v_position_entry".text)
+	playerframe.size.x = int($"interface_menu_tabs/Single Frames/values/playerframe_hp_width_entry".text)
+	playerframe.size.y = int($"interface_menu_tabs/Single Frames/values/playerframe_hp_height_entry".text)
+	playerframe.position.x = int($"interface_menu_tabs/Single Frames/values/playerframe_hp_h_position_entry".text)
+	playerframe.position.y = int($"interface_menu_tabs/Single Frames/values/playerframe_hp_v_position_entry".text)
+	playerframe.fill_mode = $"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_orientation_entry".selected
 
 func apply_int_check(value_node):
 	for child in value_node.get_children():
--- Scripts/UI/ui_persistent.gd
+++ Scripts/UI/ui_persistent.gd
@@ -2,3 +2,7 @@ extends Control
 
 func _process(delta):
 	$playerframe/playerframe_hpbar.value = 100 * Autoload.player_reference.player_hp_current / Autoload.player_reference.player_hp_max
+
+func partyframe_initialize():
+	pass
+ 
