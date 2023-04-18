59124772c5f1ab3aac3566dfb980ed309a9bd49d
cheese <alavillamailla@gmx.ch>
2023-04-18 03:31:19 +0200

    added player primary resource customization functionality

--- Scripts/UI/interface_menu.gd
+++ Scripts/UI/interface_menu.gd
@@ -4,28 +4,44 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# player frame
-	var playerframe = Autoload.player_ui_main_reference.get_node("ui_persistent").get_node("playerframe").get_node("playerframe_hpbar")
-	$"interface_menu_tabs/Single Frames/values/playerframe_hp_width_entry".text = str(playerframe.size.x)
-	$"interface_menu_tabs/Single Frames/values/playerframe_hp_height_entry".text = str(playerframe.size.y)
-	$"interface_menu_tabs/Single Frames/values/playerframe_hp_h_position_entry".text = str(playerframe.position.x)
-	$"interface_menu_tabs/Single Frames/values/playerframe_hp_v_position_entry".text = str(playerframe.position.y)
+	var player_hpbar = Autoload.player_ui_main_reference.get_node("ui_persistent").get_node("playerframe").get_node("playerframe_hpbar")
+	$"interface_menu_tabs/Single Frames/values/playerframe_hp_width_entry".text = str(player_hpbar.size.x)
+	$"interface_menu_tabs/Single Frames/values/playerframe_hp_height_entry".text = str(player_hpbar.size.y)
+	$"interface_menu_tabs/Single Frames/values/playerframe_hp_h_position_entry".text = str(player_hpbar.position.x)
+	$"interface_menu_tabs/Single Frames/values/playerframe_hp_v_position_entry".text = str(player_hpbar.position.y)
 	add_item_showhide($"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_showtoggle")
 	add_item_orientation($"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_orientation_entry")
-	$"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_showtoggle".selected = playerframe.visible
-	$"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_orientation_entry".selected = playerframe.fill_mode
+	$"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_showtoggle".selected = player_hpbar.visible
+	$"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_orientation_entry".selected = player_hpbar.fill_mode
+	var player_resbar = Autoload.player_ui_main_reference.get_node("ui_persistent").get_node("playerframe").get_node("playerframe_resbar")
+	$"interface_menu_tabs/Single Frames/values/playerframe_res_width_entry".text = str(player_resbar.size.x)
+	$"interface_menu_tabs/Single Frames/values/playerframe_res_height_entry".text = str(player_resbar.size.y)
+	$"interface_menu_tabs/Single Frames/values/playerframe_res_h_position_entry".text = str(player_resbar.position.x)
+	$"interface_menu_tabs/Single Frames/values/playerframe_res_v_position_entry".text = str(player_resbar.position.y)
+	add_item_showhide($"interface_menu_tabs/Single Frames/dropdowns/playerframe_res_showtoggle")
+	add_item_orientation($"interface_menu_tabs/Single Frames/dropdowns/playerframe_res_orientation_entry")
+	$"interface_menu_tabs/Single Frames/dropdowns/playerframe_res_showtoggle".selected = player_resbar.visible
+	$"interface_menu_tabs/Single Frames/dropdowns/playerframe_res_orientation_entry".selected = player_resbar.fill_mode
 
 func _on_interface_menu_apply_pressed():
-	var playerframe = Autoload.player_ui_main_reference.get_node("ui_persistent").get_node("playerframe").get_node("playerframe_hpbar")
+	var player_hpbar = Autoload.player_ui_main_reference.get_node("ui_persistent").get_node("playerframe").get_node("playerframe_hpbar")
+	var player_resbar = Autoload.player_ui_main_reference.get_node("ui_persistent").get_node("playerframe").get_node("playerframe_resbar")
 	var apply_validity = apply_int_check(get_node("interface_menu_tabs").get_node("Single Frames").get_node("values"))
 	# return if not all values are convertable to int
 	if not apply_validity:
 		return
-	playerframe.size.x = int($"interface_menu_tabs/Single Frames/values/playerframe_hp_width_entry".text)
-	playerframe.size.y = int($"interface_menu_tabs/Single Frames/values/playerframe_hp_height_entry".text)
-	playerframe.position.x = int($"interface_menu_tabs/Single Frames/values/playerframe_hp_h_position_entry".text)
-	playerframe.position.y = int($"interface_menu_tabs/Single Frames/values/playerframe_hp_v_position_entry".text)
-	playerframe.fill_mode = $"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_orientation_entry".selected
-	playerframe.visible = $"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_showtoggle".selected
+	player_hpbar.size.x = int($"interface_menu_tabs/Single Frames/values/playerframe_hp_width_entry".text)
+	player_hpbar.size.y = int($"interface_menu_tabs/Single Frames/values/playerframe_hp_height_entry".text)
+	player_hpbar.position.x = int($"interface_menu_tabs/Single Frames/values/playerframe_hp_h_position_entry".text)
+	player_hpbar.position.y = int($"interface_menu_tabs/Single Frames/values/playerframe_hp_v_position_entry".text)
+	player_hpbar.fill_mode = $"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_orientation_entry".selected
+	player_hpbar.visible = $"interface_menu_tabs/Single Frames/dropdowns/playerframe_hp_showtoggle".selected
+	player_resbar.size.x = int($"interface_menu_tabs/Single Frames/values/playerframe_res_width_entry".text)
+	player_resbar.size.y = int($"interface_menu_tabs/Single Frames/values/playerframe_res_height_entry".text)
+	player_resbar.position.x = int($"interface_menu_tabs/Single Frames/values/playerframe_res_h_position_entry".text)
+	player_resbar.position.y = int($"interface_menu_tabs/Single Frames/values/playerframe_res_v_position_entry".text)
+	player_resbar.fill_mode = $"interface_menu_tabs/Single Frames/dropdowns/playerframe_res_orientation_entry".selected
+	player_resbar.visible = $"interface_menu_tabs/Single Frames/dropdowns/playerframe_res_showtoggle".selected
 
 func apply_int_check(value_node):
 	for child in value_node.get_children():
