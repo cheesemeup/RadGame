4eac8c6bc1b13e5dd428110916826a248812e45b
cheese <cheese@shiny.space>
2023-06-11 01:31:52 +0200

    combat script no longer has to load the spell database, because all relevant information comes from the spell source

--- Scripts/Functionalities/combat.gd
+++ Scripts/Functionalities/combat.gd
@@ -1,12 +1,12 @@
 extends Node
 
 # test dict
-var spell_db
+#var spell_db
 
-func _ready():
-	var file = "res://Data/db_spells.json"
-	var spell_db_string = FileAccess.get_file_as_string(file)
-	spell_db = JSON.parse_string(spell_db_string)
+#func _ready():
+#	var file = "res://Data/db_spells.json"
+#	var spell_db_string = FileAccess.get_file_as_string(file)
+#	spell_db = JSON.parse_string(spell_db_string)
 
 func combat_event(spell,source,target):
 	print("combat event using %s from %s to %s"%[spell["name"],source,target])
@@ -14,19 +14,11 @@ func combat_event(spell,source,target):
 	# error handling to be implemented
 	if spell["spelltype"] == "damage":
 		event_damage(spell,source,target)
-#	elif spelltype == "dot":
-#		event_dot()
 	elif spell["spelltype"] == "heal":
 		event_healing(spell,source,target)
-#	elif spelltype == "hot":
-#		event_hot()
 #	elif spelltype == "aura":
 #		event_aura()
-	# get spell information
-	# get damage modifiers
-	# get mitigation modifiers
-	pass
-
+	
 func event_damage(spell,source,target):
 	# calculate and apply damage
 	var value = int(floor(source.stats_curr["primary"] * \
@@ -38,10 +30,6 @@ func event_damage(spell,source,target):
 	print("%s hits %s with %s for %.f damage."%\
 		[source.stats_curr["name"],target.stats_curr["name"],\
 		  spell["name"],value])
-	
-	
-func event_dot():
-	pass
 	
 func event_healing(spell,source,target):
 	# calculate and apply healing
@@ -55,8 +43,6 @@ func event_healing(spell,source,target):
 		[source.stats_curr["name"],target.stats_curr["name"],\
 		  spell["name"],value])
 	
-func event_hot():
-	pass
-	
 func event_aura():
+	# this function applies aura scenes
 	pass
