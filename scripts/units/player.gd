a7f6eaf1386d5d72633535e9023db5d519d56d6e
cheese <cheese@shiny.space>
2023-10-22 20:59:29 +0200

    .

--- scripts/units/player.gd
+++ scripts/units/player.gd
@@ -40,12 +40,17 @@ func call_set_mp_authority(peer_id):
 	input.set_multiplayer_authority(peer_id)
 	print("authority for player_input passed to peer %s" % peer_id)
 
+#func _enter_tree():
+#	# set authority
+#	rpc("call_set_mp_authority",int(self.name))
+
+func pre_enter_tree(peer_id):
+	rpc("call_set_mp_authority",peer_id)
+
 func post_ready(peer_id):
 	# some things should be done after _ready is finished
 	# have only peer do _process on input node
 	rpc_id(peer_id,"call_set_input_process",true)
-	# set input authority
-	rpc("call_set_mp_authority",peer_id)
 	# initialize ui on player
 
 func _ready():
