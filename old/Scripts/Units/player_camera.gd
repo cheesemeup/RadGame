b0e1105c481028fab5151770ac170be9de9aa71e
cheese <cheese@shiny.space>
2023-11-08 23:01:39 +0100

    camera added successfully, follows player movement

--- old/Scripts/Units/player_camera.gd
+++ old/Scripts/Units/player_camera.gd
@@ -10,8 +10,8 @@ var h_acceleration = 10
 var v_acceleration = 10
 var mouse_position
 
-func _ready():
-	print("camera added for ", self.get_parent().name)
+###########################
+## IMPLEMENT CAMERA FUNCTIONALITY
 
 #func _input(event):
 #	if not %mpsynchronizer.is_multiplayer_authority(): return
