6665a71e58905e4dceb6e48e827b7cc3ac95d05b
cheese <cheese@shiny.space>
2023-11-09 20:06:00 +0100

    different simplification

--- old/Scripts/Units/player_camera.gd
+++ old/Scripts/Units/player_camera.gd
@@ -14,9 +14,8 @@ var mouse_position
 func _input(event):
 	# move camera if rightlick pressed and mouse moves
 	if event is InputEventMouseMotion and Input.is_action_pressed("rightclick"):
-		camrot_v += -event.relative.y * v_sensitivity
-		self.rotation_degrees.y = -event.relative.x * h_sensitivity
-		self.rotation_degrees.x = clamp(camrot_v, cam_v_min, cam_v_max)
+		self.rotation_degrees.y -= event.relative.x * h_sensitivity
+		self.rotation_degrees.x += clamp(-event.relative.y * v_sensitivity, cam_v_min, cam_v_max)
 
 	# get mouse position when click is initiated and hide cursor
 	if event is InputEventMouseButton and Input.is_action_pressed("rightclick"):
