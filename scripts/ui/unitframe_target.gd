5b211ccf6c348c02f91e514867e400e5c596057f
cheese <cheese@shiny.space>
2024-02-13 22:10:09 +0100

    debug messaging

--- scripts/ui/unitframe_target.gd
+++ scripts/ui/unitframe_target.gd
@@ -6,6 +6,10 @@ func _process(delta):
 	if target_reference == null:
 		return
 	# update health bar
+	print(target_reference)
+	print(target_reference.stats)
+	print(target_reference.stats.stats_current)
+	print(target_reference.stats.stats_current.health_current)
 	$hpbar_value.value = 100 * target_reference.stats.stats_current.health_current / target_reference.stats.stats_current.health_max
 	# update resource bar
 	$resourcebar_value.value = 100 * target_reference.stats.stats_current.resource_current / target_reference.stats.stats_current.resource_max
