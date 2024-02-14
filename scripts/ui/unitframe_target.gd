ac805915c1befc236e25913c823d54ff8c9daebe
cheese <cheese@shiny.space>
2024-02-14 18:44:30 +0100

    more testing for value display

--- scripts/ui/unitframe_target.gd
+++ scripts/ui/unitframe_target.gd
@@ -6,6 +6,7 @@ func _process(delta):
 	# return if there is no target set
 	if target_reference == null:
 		return
+	print("of course it is being processed")
 	# update health bar
 	$hpbar_value.value = 100 * target_reference.stats_current.health_current / target_reference.stats_current.health_max
 	$"hpbar_value/health".text = "%s/%s | %.2f%s" % \
