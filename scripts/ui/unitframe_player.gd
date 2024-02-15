ea7fc451230d1de28fd7bcb893d9e9f22c08809f
cheesemeup <139981646+cheesemeup@users.noreply.github.com>
2024-02-15 02:17:09 +0100

    playerframe (#12)
    * begin playerframe
    
    * added playerframe scene
    
    * playerframe positioning
    
    * fix
    
    * fix
    
    * fix
    
    * resource bar test
    
    * playerframe functional
    
    ---------
    
    Co-authored-by: cheese <cheese@shiny.space>

---
+++ scripts/ui/unitframe_player.gd
@@ -0,0 +1,21 @@
+extends Control
+
+var player_reference = null
+
+func _ready():
+	player_reference = Autoload.player_reference
+	$"hpbar_value/unitname".text = player_reference.stats_current["unit_name"]
+
+func _process(delta):
+	# update health bar
+	$hpbar_value.value = 100 * player_reference.stats_current.health_current / player_reference.stats_current.health_max
+	$"hpbar_value/health".text = "%s/%s" % \
+		[player_reference.stats_current.health_current, player_reference.stats_current.health_max]
+	$"hpbar_value/healthpercent".text = "%.2f%s" % \
+		[float(100*player_reference.stats_current.health_current/player_reference.stats_current.health_max), "%"]
+	# update resource bar
+	$resourcebar_value.value = 100 * player_reference.stats_current.resource_current / player_reference.stats_current.resource_max
+	$"resourcebar_value/resource".text = "%s/%s" % \
+		[player_reference.stats_current.resource_current, player_reference.stats_current.resource_max]
+	$"resourcebar_value/resourcepercent".text = "%.2f%s" % \
+		[float(100*player_reference.stats_current.resource_current/player_reference.stats_current.resource_max), "%"]
--- scripts/ui_handler.gd
+++ scripts/ui_handler.gd
@@ -1,14 +1,25 @@
 # This script loads, unloads, shows and hides UI elements
 extends Node
 
+# playerframe
+var playerframe
+var playerframe_hp_x = 1130  # x position
+var playerframe_hp_y = 735  # y position
+var playerframe_hp_w = 220  # width
+var playerframe_hp_h = 70  # height
+var playerframe_res_x = 840  # x position
+var playerframe_res_y = 735  # y position
+var playerframe_res_w = 240  # width
+var playerframe_res_h = 40  # height
+
 # targetframe
 var targetframe
-var targetframe_hp_x = 1080  # x position
-var targetframe_hp_y = 795  # y position
+var targetframe_hp_x = 1130  # x position
+var targetframe_hp_y = 805  # y position
 var targetframe_hp_w = 220  # width
 var targetframe_hp_h = 60  # height
-var targetframe_res_x = 1080  # x position
-var targetframe_res_y = 855  # y position
+var targetframe_res_x = 1130  # x position
+var targetframe_res_y = 865  # y position
 var targetframe_res_w = 220  # width
 var targetframe_res_h = 25  # height
 
@@ -22,7 +33,25 @@ func load_mainmenu():
 ####################################################################################################
 # UNITFRAMES
 func load_unitframes():
+	initialize_playerframe()
 	initialize_targetframe()
+
+# PLAYERFRAME
+func initialize_playerframe():
+	playerframe = preload("res://scenes/ui/unitframe_player.tscn")
+	playerframe = playerframe.instantiate()
+	set_playerframe_position()
+	$"/root/main/ui".add_child(playerframe)
+func set_playerframe_position():
+	playerframe.get_node("hpbar_value").size = Vector2(playerframe_hp_w,playerframe_hp_h)
+	playerframe.get_node("hpbar_value").position = Vector2(playerframe_hp_x,playerframe_hp_y)
+	playerframe.get_node("hpbar_value").get_node("unitname").size = Vector2(playerframe_hp_w,playerframe_hp_h)
+	playerframe.get_node("hpbar_value").get_node("health").size = Vector2(playerframe_hp_w,playerframe_hp_h)
+	playerframe.get_node("hpbar_value").get_node("healthpercent").size = Vector2(playerframe_hp_w,playerframe_hp_h)
+	playerframe.get_node("resourcebar_value").size = Vector2(playerframe_res_w,playerframe_res_h)
+	playerframe.get_node("resourcebar_value").position = Vector2(playerframe_res_x,playerframe_res_y)
+	playerframe.get_node("resourcebar_value").get_node("resource").size = Vector2(playerframe_res_w,playerframe_res_h)
+	playerframe.get_node("resourcebar_value").get_node("resourcepercent").size = Vector2(playerframe_res_w,playerframe_res_h)
 
 # TARGEFRAME
 func initialize_targetframe():
@@ -32,7 +61,6 @@ func initialize_targetframe():
 	set_targetframe_position()
 	$"/root/main/ui".add_child(targetframe)
 func set_targetframe_position():
-	# positioning
 	targetframe.get_node("hpbar_value").size = Vector2(targetframe_hp_w,targetframe_hp_h)
 	targetframe.get_node("hpbar_value").position = Vector2(targetframe_hp_x,targetframe_hp_y)
 	targetframe.get_node("hpbar_value").get_node("unitname").size = Vector2(targetframe_hp_w,targetframe_hp_h)
