596f24f6bea379ce58c8e8e1d0680391c5e459ab
cheese <cheese@shiny.space>
2024-02-16 16:04:08 +0100

    testing if forcing readable name fixes issue

--- scripts/main.gd
+++ scripts/main.gd
@@ -13,11 +13,11 @@ func _ready():
 		var test_npc = preload("res://scenes/testing/test_npc_hostile.tscn").instantiate()
 		test_npc.set_process(false)
 		test_npc.position = Vector3(-6,0,-6)
-		$npcs.add_child(test_npc)
+		$npcs.add_child(test_npc,true)
 		test_npc = preload("res://scenes/testing/test_npc_hostile.tscn").instantiate()
 		test_npc.set_process(false)
 		test_npc.position = Vector3(-3,0,-6)
-		$npcs.add_child(test_npc)
+		$npcs.add_child(test_npc,true)
 		print("map loaded")
 		return
 	Autoload.main_reference = self
