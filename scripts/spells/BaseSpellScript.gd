d393d0819e7550ea9565e8ca7a884a8442e9f36f
cheese <cheese@shiny.space>
2024-02-17 10:52:10 +0100

    added a few checks in the basespell class

--- scripts/spells/BaseSpellScript.gd
+++ scripts/spells/BaseSpellScript.gd
@@ -6,35 +6,42 @@ var spell_base: Dictionary
 var spell_current: Dictionary
 var result_strings
 var cd_timer = Timer.new()
-var on_cd = false
 
 func _ready():
 	cd_timer.one_shot = true
-	cd_timer.connect("timeout",set_ready.bind())
 	add_child(cd_timer)
 
 func initialize_base_spell(spell_id: String):
 	var json_dict = JSON.parse_string(FileAccess.get_file_as_string("res://data/db_spells.json"))
 	spell_base = json_dict[spell_id]
 	spell_current = spell_base.duplicate(true)
-	result_strings = [
-		"insufficient resources",
-		"invalid target",
-		"out of range"
-	]
+	# connect gcd signal if spell is on gcd
+	if spell_current["on_gcd"] == 1:
+		get_parent().signal_gcd.connect(trigger_cd)
 
 ####################################################################################################
 # CHECKS
+func is_on_cd():
+	if cd_timer.time_left > 0:
+		return true
+	return false
+func insufficient_resource(cost: int, resource: int):
+	if cost < resource:
+		return true
+	return false
+func is_not_in_range(source_position: Vector3, target_position: Vector3, range: float):
+	if source_position.distance_to(target_position) > range:
+		return true
+	return false
+func is_not_in_line_of_sight():
+	pass
 
 ####################################################################################################
 # COOLDOWN
-func trigger_cd(duration):
+func trigger_cd(duration: float):
 	# check if current cooldown exceeds requested cooldown
 	if cd_timer.time_left > duration:
 		return
 	# start timer
 	cd_timer.wait_time = duration
 	cd_timer.start()
-	on_cd = true
-func set_ready():
-	on_cd = false
--- scripts/spells/spell_10.gd
+++ scripts/spells/spell_10.gd
@@ -3,73 +3,27 @@ extends BaseSpell
 
 func _ready():
 	initialize_base_spell("10")
-	cd_timer.one_shot = true
-	cd_timer.connect("timeout",set_ready.bind())
-	add_child(cd_timer)
 	# connect to gcd signal of spell container
-	get_parent().signal_gcd.connect(trigger_cd)
 
 func trigger():
 	# PUT A LOT OF THIS INTO BASE SPELL CLASS
 	# get source and target nodes
-	var sourcenode = get_parent().get_parent()
+	var source = get_parent().get_parent()
+	var target = 0
 	# check for cooldown
+	if is_on_cd():
+		return 1
 	# check resource availability
+	if insufficient_resource(spell_current["resource_cost"],\
+								source.stats_current["resource_current"]):
+		return 2
 	# check range
+	if is_not_in_range(source.position,target.position,spell_current["range"]):
+		return 3
 	# check line of sight
+	if is_not_in_line_of_sight():
+		return 3
 	# apply resource cost
 	# send gcd
 	# apply spell cd if not on gcd
 	# send event to combat script
-	
-	
-	
-	
-	# check cooldown
-	if on_cd:
-		print("on cooldown")
-		return
-	# check target
-	var spell_target = sourcenode.get_spell_target(spell_curr)
-	if typeof(spell_target) == TYPE_STRING and spell_target == "no_legal_target":
-		print("no legal target")
-		return
-	# check range
-	if sourcenode.global_transform.origin.distance_to(spell_target.global_transform.origin) - spell_target.stats_curr["size"] > spell_curr["range"]:
-		print("out of range")
-		return
-	# apply resource cost
-	sourcenode.stats_curr["resource_current"] = min(sourcenode.stats_curr["resource_current"]-spell_curr["resource_cost"],sourcenode.stats_curr["resource_max"])
-	# send gcd
-	get_parent().send_gcd()
-	# fire spell
-	Combat.event_damage(spell_curr,sourcenode,spell_target)
-
-func trigger_cd(duration):
-	# start timer
-	cd_timer.wait_time = duration
-	cd_timer.start()
-	on_cd = true
-	# start cd swipe
-	for ab in actionbar:
-		ab.start_cd(duration)
-
-func set_ready():
-	on_cd = false
-	for ab in actionbar:
-		ab.end_cd()
-
-# role swap effects
-func swap_tank():
-	spell_curr["damagetype"] = "magic"
-func swap_heal():
-	spell_curr["damagetype"] = "magic"
-	spell_curr["resource_cost"] = 0
-	spell_curr["range"] = 30
-func swap_meleedps():
-	spell_curr["damagetype"] = "physical"
-func swap_rangedps():
-	spell_curr["damagetype"] = "magic"
-	spell_curr["range"] = 30
-
-# talent effects  
