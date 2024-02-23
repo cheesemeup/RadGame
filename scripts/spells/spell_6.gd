c8898638f902c4eabe3fab79213a0b50fb88ed96
cheese <cheese@shiny.space>
2024-02-23 15:05:41 +0100

    debug messages

--- scripts/spells/spell_6.gd
+++ scripts/spells/spell_6.gd
@@ -13,18 +13,23 @@ func trigger():
 		target = source
 	# check target legality
 	if is_illegal_target(spell_current["targetgroup"], target):
+		print("illegal target")
 		return 1
 	# check for cooldown
 	if is_on_cd():
+		print("on cd")
 		return 2
 	# check range
 	if is_not_in_range(source.position,target.position,spell_current["max_range"]):
+		print("not in range")
 		return 4
 	# check line of sight, NOT FUNCTIONAL
 	if is_not_in_line_of_sight(source,target.position):
+		print("not in line of sight")
 		return 5
 	# send gcd
 	if spell_current["on_gcd"] == 1:
 		get_parent().send_gcd()
 	# send event to combat script
+	print("sending combat event")
 	Combat.combat_event_entrypoint(spell_current,source,target)
