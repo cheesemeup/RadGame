e058ab0a2a5f5dd2a62be8f5c75dac3d0e1f82af
cheese <cheese@shiny.space>
2024-02-23 23:13:37 +0100

    test player test hot

--- scripts/spells/spell_7.gd
+++ scripts/spells/spell_7.gd
@@ -1,45 +1,35 @@
-extends Node
-
-# Player Test HoT
-var spell_base : Dictionary
-var spell_curr : Dictionary
-var cd_timer = Timer.new()
-var on_cd = false
+# Player Test Hot
+extends BaseSpell
 
 func _ready():
-	var json_dict = JSON.parse_string(FileAccess.get_file_as_string("res://data/db_spells.json"))
-	spell_base = json_dict["7"]
-	spell_curr = spell_base.duplicate(true)
-	cd_timer.one_shot = true
-	cd_timer.connect("timeout",set_ready.bind())
-	add_child(cd_timer)
+	initialize_base_spell("7")
 
 func trigger():
-	var sourcenode = get_parent().get_parent()
-	# check resource cost
-	if sourcenode.stats_curr["resource_current"] < spell_curr["resource_cost"]:
-		print("insufficient resources")
-		return
-	# check target
-	var spell_target = sourcenode.get_spell_target(spell_curr)
-	if typeof(spell_target) == TYPE_STRING and spell_target == "no_legal_target":
-		print("no legal target")
-		return
+	# get source and target nodes
+	var source = get_parent().get_parent()
+	var target = get_spell_target(source)
+	# set target to self if there is no target
+	if target == null:
+		target = source
+	# check target legality
+	if is_illegal_target(spell_current["targetgroup"], target):
+		print("illegal target")
+		return 1
+	# check for cooldown
+	if is_on_cd():
+		print("on cd")
+		return 2
 	# check range
-	if sourcenode.global_transform.origin.distance_to(spell_target.global_transform.origin) - spell_target.stats_curr["size"] > spell_curr["range"]:
-		print("out of range")
-		return
-	# apply resource cost
-	sourcenode.stats_curr["resource_current"] -= spell_curr["resource_cost"]
+	if is_not_in_range(source.position,target.position,spell_current["max_range"]):
+		print("not in range")
+		return 4
+	# check line of sight, NOT FUNCTIONAL
+	if is_not_in_line_of_sight(source,target.position):
+		print("not in line of sight")
+		return 5
 	# send gcd
-	get_parent().send_gcd()
-	# fire spell
-	Combat.event_aura(spell_curr,sourcenode,spell_target)
-
-func start_cd(duration):
-	cd_timer.wait_time = duration
-	cd_timer.start()
-	on_cd = true
-
-func set_ready():
-	on_cd = false
+	if spell_current["on_gcd"] == 1:
+		get_parent().send_gcd()
+	# send event to combat script
+	print("sending combat event")
+	Combat.combat_event_aura_entrypoint(spell_current,source,target)
