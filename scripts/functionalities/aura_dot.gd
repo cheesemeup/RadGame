5cdfa44479ae621006f5f6f71deae7d672e075bc
cheese <cheese@shiny.space>
2024-02-23 16:09:01 +0100

    checking timer again

--- scripts/functionalities/aura_dot.gd
+++ scripts/functionalities/aura_dot.gd
@@ -18,10 +18,10 @@ func initialize(spell,source,target):
 	tickrate = spell["tickrate"]
 	print("init timer")
 	# initialize timer
-	#tick_timer.new()
-	#tick_timer.wait_time = tickrate
-	#tick_timer.connect("timeout",tick)
-	#add_child(tick_timer)
+	tick_timer.new()
+	tick_timer.wait_time = tickrate
+	tick_timer.connect("timeout",tick)
+	add_child(tick_timer)
 
 # start timer when ready
 func _ready():
