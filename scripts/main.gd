1407a90b9c96055128e7625699d0d8cbb1ead7ae
cheese <cheese@shiny.space>
2024-03-14 22:17:06 +0100

    spawn testing

--- scripts/main.gd
+++ scripts/main.gd
@@ -19,7 +19,7 @@ func _ready():
 		$/root/main/npcs.add_child(test_npc,true)
 		var test_signpost_damage = preload("res://scenes/testing/interact_damage.tscn").instantiate()
 		test_signpost_damage.position = Vector3(0,0,-6)
-		$/root/main/npcs.add_child(test_signpost_damage,true)
+		$/root/main/interactables.add_child(test_signpost_damage,true)
 		return
 	Autoload.main_reference = self
 	# load main menu for players
