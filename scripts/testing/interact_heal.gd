c6be85c248dd525ffb819402be3057c753abb70e
cheese <cheese@shiny.space>
2024-03-16 21:01:26 +0100

    hotfix

--- scripts/testing/interact_heal.gd
+++ scripts/testing/interact_heal.gd
@@ -1,20 +1,20 @@
 extends BaseUnit
 
-var stats_base: Dictionary
-@export var stats_curr: Dictionary
-var spells_base: Dictionary
-@export var spells_curr: Dictionary
-
-func _ready():
-	# load stats and spells
-	var file = "res://data/db_stats_npc.json"
-	var json_dict = JSON.parse_string(FileAccess.get_file_as_string(file))
-	stats_base = json_dict["1"]
-	stats_curr = stats_base
-	file = "res://data/db_spells.json"
-	json_dict = JSON.parse_string(FileAccess.get_file_as_string(file))
-	spells_base["0"] = json_dict["1"]
-	spells_curr = spells_base
-
-func interaction(body):
-	Combat.event_heal(spells_curr["0"],self,body)
+#var stats_base: Dictionary
+#@export var stats_curr: Dictionary
+#var spells_base: Dictionary
+#@export var spells_curr: Dictionary
+#
+#func _ready():
+	## load stats and spells
+	#var file = "res://data/db_stats_npc.json"
+	#var json_dict = JSON.parse_string(FileAccess.get_file_as_string(file))
+	#stats_base = json_dict["1"]
+	#stats_curr = stats_base
+	#file = "res://data/db_spells.json"
+	#json_dict = JSON.parse_string(FileAccess.get_file_as_string(file))
+	#spells_base["0"] = json_dict["1"]
+	#spells_curr = spells_base
+#
+#func interaction(body):
+	#Combat.event_heal(spells_curr["0"],self,body)
