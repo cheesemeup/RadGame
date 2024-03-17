b772330ef905f56581abb94c0ef1373cad312bdb
cheese <cheese@shiny.space>
2024-03-17 14:38:21 +0100

    signpost heal test

--- scripts/testing/interact_damage.gd
+++ scripts/testing/interact_damage.gd
@@ -15,13 +15,3 @@ func trigger(interactor):
 	Combat.log_interact(interactor.stats_current["unit_name"],self.stats_current["unit_name"])
 	# trigger spell ID 0
 	$"spell_container/spell_0".trigger(interactor)
-
-#func ready_server():
-	#initialize_base_interactable("0")
-
-#func interaction(body):
-	#if not multiplayer.is_server():
-		#return
-	## source and target are flipped here, because the source of the interaction
-	## (the player) turns into the target of the spell
-	#$spell_container/spell_0.trigger(self,body)
--- scripts/testing/interact_heal.gd
+++ scripts/testing/interact_heal.gd
@@ -9,17 +9,9 @@ func _ready():
 	if not $mpsynchronizer.is_multiplayer_authority():
 		return
 	initialize_base_interactable("1")
-	$"range/range_shape".shape.radius = 3
-	#set_process(false)
-	#if $mpsynchronizer.is_multiplayer_authority():
-		#set_process(true)
 
-#func ready_server():
-	#initialize_base_interactable("0")
-
-#func interaction(body):
-	#if not multiplayer.is_server():
-		#return
-	## source and target are flipped here, because the source of the interaction
-	## (the player) turns into the target of the spell
-	#$spell_container/spell_0.trigger(self,body)
+func trigger(interactor):
+	# write interaction to log
+	Combat.log_interact(interactor.stats_current["unit_name"],self.stats_current["unit_name"])
+	# trigger spell ID 1
+	$"spell_container/spell_1".trigger(interactor)
