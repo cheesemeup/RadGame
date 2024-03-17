dc9171fd8523c76d2e2b8a97ac569b58a0fd883c
cheese <cheese@shiny.space>
2024-03-17 16:59:48 +0100

    interact prompt hotkey

--- scripts/units/player.gd
+++ scripts/units/player.gd
@@ -136,7 +136,6 @@ func get_nearest_interactable():
 	# trigger removal and reapplication of interact prompt if nearest interactable changes
 	if nearest_interactable != current_interactable:
 		if current_interactable != null:
-			print(InputMap.action_get_events("interact")[0].as_text())
 			rpc_id(name.to_int(),"hide_interact_prompt",current_interactable.name)
 		if nearest_interactable != null:
 			rpc_id(name.to_int(),"show_interact_prompt",nearest_interactable.name)
@@ -145,9 +144,9 @@ func get_nearest_interactable():
 @rpc("authority")
 func show_interact_prompt(interactable_name: String):
 	# rpc that makes interact prompt visible locally
-	$"/root/main/interactables".get_node(interactable_name).\
-	get_node("interact_prompt").text = "Interact [%s]"%\
-		InputMap.action_get_events("interact")[0].as_text()
+	#$"/root/main/interactables".get_node(interactable_name).\
+	#get_node("interact_prompt").text = "Interact [%s]"%\
+		#InputMap.action_get_events("interact")[0].as_text()
 	$"/root/main/interactables".get_node(interactable_name).\
 	get_node("interact_prompt").visible = true
 @rpc("authority")
