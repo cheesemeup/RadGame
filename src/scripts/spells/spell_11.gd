1bb2f7de8374b97dd33bb0613df613cfd909267a
cheese <cheese@shiny.space>
2024-03-18 21:27:46 +0100

    debugging

--- src/scripts/spells/spell_11.gd
+++ src/scripts/spells/spell_11.gd
@@ -5,17 +5,20 @@ func _ready():
 	initialize_base_spell("11")
 
 func trigger():
+	print("trigger function")
 	# get source and target nodes
 	var source = get_parent().get_parent()
 	# check for cooldown
 	if is_on_cd():
 		return 2
+	print("not on cd")
 	# check resource availability
 	if insufficient_resource(
 		spell_current["resource_cost"],
 		source.stats_current["resource_current"]
 	):
 		return 3
+	print("sufficient resources")
 	# apply resource cost 
 	source.stats_current["resource_current"] = update_resource(
 		spell_current["resource_cost"],
@@ -25,5 +28,7 @@ func trigger():
 	# send gcd
 	if spell_current["on_gcd"] == 1:
 		get_parent().send_gcd()
+	print("not on cd")
 	# send event to combat script
+	print("send event")
 	Combat.combat_event_aura_entrypoint(spell_current,source,source)
