1e36a9df20e79862a31335841455e9ce4953f0a6
cheese <cheese@shiny.space>
2024-06-23 19:35:45 +0200

    spawn location test

--- src/scripts/server/serverscript.gd
+++ src/scripts/server/serverscript.gd
@@ -70,7 +70,7 @@ func map_swap(map_name: String):
 	for player in $/root/main/players.get_children():
 		if not player.is_in_group("player"):
 			continue
-		player.translation = map_instance.initial_spawn_position
+		player.global_position = map_instance.initial_spawn_position
 		rpc("enable_player",player.name)
 
 
