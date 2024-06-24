42783c7a16bf813ce547dbf4b694c008a22bd81d
cheese <cheese@shiny.space>
2024-06-25 01:05:44 +0200

    target dummy position test

---
+++ src/scripts/units/targetdummy_dps.gd
@@ -0,0 +1,16 @@
+extends BaseUnit
+
+func _enter_tree():
+	# only have server process
+	$mpsynchronizer.set_multiplayer_authority(1)
+
+func _ready() -> void:
+	# initialize BaseUnit
+	initialize_base_unit("npc","3")
+	set_process(false)
+	if $mpsynchronizer.is_multiplayer_authority():
+		set_process(true)
+
+func _process(_delta: float) -> void:
+	if stats_current["health_current"] == stats_current["health_max"]:
+		$"spell_container/spell_4".trigger()
