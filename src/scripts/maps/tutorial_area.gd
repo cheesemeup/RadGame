7be2f7ef5be179d58a0644c061c820efe11f6a05
cheese <cheese@shiny.space>
2024-06-25 02:07:05 +0200

    targetdummy spawn via code test

--- src/scripts/maps/tutorial_area.gd
+++ src/scripts/maps/tutorial_area.gd
@@ -11,7 +11,5 @@ func initialize():
 func init_npcs():
 	# NPCs must be spawned via code to ensure forced readable names
 	var dummy_instance = targetdummy_dps.instantiate()
-	dummy_instance.name = "targetdummy_dps_singletarget"
 	dummy_instance.position = Vector3(-8,-5,27)
 	$npcs.add_child(dummy_instance,true)
-	pass
