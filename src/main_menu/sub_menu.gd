df89e11a10d9579432eed06d5ee1254c00f221dc
miejs <mie-js@outlook.com>
2024-07-01 00:27:09 +0200

    simplified main menu and sub menu navigation

--- src/main_menu/sub_menu.gd
+++ src/main_menu/sub_menu.gd
@@ -4,12 +4,20 @@ class_name SubMenu
 @export var title: String
 @export var sub_menu_scene: PackedScene
 
-func instantiate_hidden() -> Node:
-	var sub_menu = sub_menu_scene.instantiate()
-	sub_menu.hide()
-	return sub_menu
+const BACK_BUTTON = preload("res://main_menu/back_button.tscn")
 
-func get_button() -> Button:
+func get_sub_menu_title() -> Label:
+	var titleLabel = Label.new()
+	titleLabel.text = title
+	titleLabel.theme_type_variation = "HeaderLarge"
+	return titleLabel
+
+func get_sub_menu_button() -> Button:
 	var button = Button.new()
 	button.text = title
+	return button
+
+func get_back_button(connect_to: Callable) -> Button:
+	var button = BACK_BUTTON.instantiate()
+	button.pressed.connect(connect_to)
 	return button
