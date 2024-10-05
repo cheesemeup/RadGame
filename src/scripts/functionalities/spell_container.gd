c694480720a6d5585a32e1851ec4a448532d5e51
cheese <cheese@shiny.space>
2024-10-05 23:23:51 +0200

    GCD 1.5 seconds again, spell queueing works after instant spells that trigger gcd, and with casts taht are 1.5 long

--- src/scripts/functionalities/spell_container.gd
+++ src/scripts/functionalities/spell_container.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var player = get_parent()
-var gcd_timer: float = 1.49
+var gcd_timer: float = 1.5
 var result: int
 var queue: String = ""
 var queue_instant: Array = []
@@ -27,3 +27,24 @@ func spell_entrypoint(spell_id: String) -> int:
 func send_gcd() -> void:
 	# emit signal, true to denote gcd
 	signal_gcd.emit(gcd_timer,true)
+
+
+func set_queue(ID: String) -> void:
+	if get_node("spell_%s"%ID).cd_timer.is_connected("timeout",cast_queued):
+		get_node("spell_%s"%ID).cd_timer.disconnect("timeout",cast_queued)
+	queue = ID
+	get_node("spell_%s"%ID).cd_timer.connect("timeout",cast_queued)
+
+
+func cast_queued() -> void:
+	# trigger queued spell if it exists, prioritizing instant casts
+	if not queue_instant == []:
+		var new_id = queue_instant.pop_front()
+		spell_entrypoint(new_id)
+		return
+	if not queue == "":
+		if get_node("spell_%s"%queue).cd_timer.is_connected("timeout",cast_queued):
+			get_node("spell_%s"%queue).cd_timer.disconnect("timeout",cast_queued)
+		var new_id = queue
+		queue = ""
+		spell_entrypoint(new_id)
