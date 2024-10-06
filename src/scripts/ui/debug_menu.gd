a9e7e1f5ce51be80123bbbe5085fde97c95358b7
miejs <15327294+miejs@users.noreply.github.com>
2024-10-06 12:54:13 +0200

    added degub option to display collision shapes in game

--- src/scripts/ui/debug_menu.gd
+++ src/scripts/ui/debug_menu.gd
@@ -1,5 +1,12 @@
 extends Node
 
 func _on_restart_map_button_pressed() -> void:
+	restart_map()
+
+func _on_toggle_collision_shapes_button_pressed() -> void:
+	get_tree().debug_collisions_hint = !get_tree().debug_collisions_hint
+	restart_map()
+
+func restart_map() -> void:
 	Serverscript.map_swap(References.current_map_name)
 	UIHandler.close_ingame_menu()
