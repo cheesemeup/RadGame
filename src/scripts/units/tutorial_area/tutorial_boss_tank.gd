2094c4db2eebb1b7fef79c6b5221d34cabe50acd
cheesemeup <139981646+cheesemeup@users.noreply.github.com>
2025-06-18 17:33:39 +0200

    movement and aggro behavior of base_hostile

---
+++ src/scripts/units/tutorial_area/tutorial_boss_tank.gd
@@ -0,0 +1,9 @@
+extends BaseHostile
+
+# dummy script for now, to test basehostile class
+
+# make tutorialboss model scene, then try spawning it in with yet another signpost
+# test spawn
+# test aggrotable and targeting
+# test autoattack
+# try pathfinding, maybe
