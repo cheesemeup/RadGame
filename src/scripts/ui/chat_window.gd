13ffb17c49d76245c94383ba5a3bc2afbd716fef
Lucas <lucas@tux4>
2025-10-25 22:21:41 +0200

    scroll to bottom only when a new message is received - deferred TWICE because once isnt enough.

--- src/scripts/ui/chat_window.gd
+++ src/scripts/ui/chat_window.gd
@@ -1,17 +1,6 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	var verticalscrollbar: VScrollBar = $FoldableContainer/VBoxContainer/hbox_messages/ScrollContainer.get_v_scroll_bar()
-	verticalscrollbar.value = verticalscrollbar.max_value
-
-
 func _on_text_edit_text_submitted(new_text: String) -> void:
 	if new_text.begins_with("/"):
 		handle_chat_command(new_text)
@@ -22,13 +11,20 @@ func _on_text_edit_text_submitted(new_text: String) -> void:
 	write_message.rpc(messageFormatted)
 	$FoldableContainer/VBoxContainer/hbox_chatbar/chat_input.clear()
 
+
 @rpc("any_peer", "call_local")
 func write_message(message: String) -> void:
 	var newmsg = Label.new()
 	newmsg.text = message
 	$FoldableContainer/VBoxContainer/hbox_messages/ScrollContainer/MessageContainer.add_child(newmsg)
+	call_deferred("scroll_to_bottom")
+	
 
 
-func handle_chat_command(command: String) -> void:
-	print("HACKED ", command)
+func handle_chat_command(_command: String) -> void:
 	References.player_reference.request_unstuck.rpc()
+
+
+func scroll_to_bottom() -> void:
+	var verticalscrollbar: VScrollBar = $FoldableContainer/VBoxContainer/hbox_messages/ScrollContainer.get_v_scroll_bar()
+	verticalscrollbar.set_deferred("value", verticalscrollbar.max_value)
