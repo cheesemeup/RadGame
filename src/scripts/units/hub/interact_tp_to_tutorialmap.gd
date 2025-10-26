6b5696e4e5a917699069afbbc0703012f31d6276
miejs <15327294+miejs@users.noreply.github.com>
2025-10-26 02:25:56 +0200

    map change with vote

--- src/scripts/units/hub/interact_tp_to_tutorialmap.gd
+++ src/scripts/units/hub/interact_tp_to_tutorialmap.gd
@@ -5,6 +5,6 @@ func custom_post_ready():
 	$pivot/active_model/sign_text.text = "Tutorialmap"
 
 
-func custom_trigger(interactor):
+func custom_trigger(_interactor):
 	# trigger map swap
 	Serverscript.request_map_swap("tutorial_area.tscn")
